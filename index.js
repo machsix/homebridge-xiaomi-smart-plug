3bf628abdf4ddb09745d99430d8a21a02ca54d43
machsix <28209092+machsix@users.noreply.github.com>
2022-04-03 04:51:55 -0700

    fix bug

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ class XiaomiSmartPlug {
         this.service = new this.Service.Outlet(this.name);
         this.service
             .getCharacteristic(this.Characteristic.On)
-            .onGet(this.getState.bind(this))
-            .onSet(this.setState.bind(this));
+            .onGet(this.handleOnGet.bind(this))
+            .onSet(this.handleOnSet.bind(this));
 
         this.checkPlugState().catch((error) => this.log.error(error));
     }
@@ -65,28 +65,27 @@ class XiaomiSmartPlug {
             });
     }
 
-    async getState(callback) {
+    async handleOnGet() {
         this.log('Get state...');
         try {
             const device = await this.getDevice();
             const power =
                 (await device.miioCall('get_prop', ['power'])) === 'on';
-            callback(null, power);
+            return power;
         } catch (e) {
             this.log.error('Error getting state', e);
-            callback(e);
+            throw e;
         }
     }
 
-    async setState(state, callback) {
+    async handleOnSet(state) {
         this.log('Set state to', state);
         try {
             const device = await this.getDevice();
             await await device.miioCall('set_power', [state ? 'on' : 'off']);
-            callback(null);
         } catch (e) {
             this.log.error('Error setting state', e);
-            callback(e);
+            throw e;
         }
     }
 
