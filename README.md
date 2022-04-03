e564e23207d0aac3a65dbdbd33fdcd35c577a755
machsix <28209092+machsix@users.noreply.github.com>
2022-04-03 04:14:13 -0700

    initial

---
+++ README.md
@@ -0,0 +1,50 @@
+[![npm version](https://badge.fury.io/js/homebridge-xiaomi-smart-plug.svg)](https://badge.fury.io/js/homebridge-xiaomi-smart-plug)
+# Homebridge Mi Smart Plug
+
+[Homebridge](https://github.com/nfarina/homebridge) plugin for Xiaomi Smart Plug
+
+![mi](https://i01.appmifile.com/webfile/globalimg/49/77AC4D29-2E0E-325D-2CC9-20E243139E71.jpg)
+
+## Supporting devices
+
+The following ones should be supported as they share the same API [https://github.com/rytilahti/python-miio/blob/master/miio/chuangmi_plug.py](https://github.com/rytilahti/python-miio/blob/master/miio/chuangmi_plug.py)
+- MODEL_CHUANGMI_PLUG_V3 = "chuangmi.plug.v3"
+- ~~MODEL_CHUANGMI_PLUG_V1 = "chuangmi.plug.v1"~~
+- MODEL_CHUANGMI_PLUG_M1 = "chuangmi.plug.m1"
+- MODEL_CHUANGMI_PLUG_M3 = "chuangmi.plug.m3"
+- MODEL_CHUANGMI_PLUG_V2 = "chuangmi.plug.v2"
+- MODEL_CHUANGMI_PLUG_HMI205 = "chuangmi.plug.hmi205" [[Walmart]](https://www.walmart.com/ip/Xiaomi-Mi-Smart-Plug-1-Pack/295288889)
+- MODEL_CHUANGMI_PLUG_HMI206 = "chuangmi.plug.hmi206"
+- MODEL_CHUANGMI_PLUG_HMI208 = "chuangmi.plug.hmi208"
+
+## Get your token and IP
+
+Good luck!!! (Because it's the hard part), some links to help you:
+
+* https://github.com/jghaanstra/com.xiaomi-miio/blob/master/docs/obtain_token.md
+* https://github.com/mediter/miio/blob/master/docs/ios-token-without-reset.md
+
+## Installation
+
+```
+npm install -g homebridge-xiaomi-smart-plug
+```
+
+
+## Example Configuration
+
+```json
+{
+  "accessories": [
+    {
+    "accessory": "homebridge-xiaomi-smart-plug", // don't change this
+    "name": "My Smart Plug",
+    "ip": "192.168.0.101",
+    "token": "6591d0ad2003ddd2da75815f5d7def26"
+    }
+  ]
+}
+```
+
+## Thanks to
+[homebridge-mi-led-desk-lamp](https://github.com/moifort/homebridge-mi-led-desk-lamp)
---
+++ index.js
@@ -0,0 +1,96 @@
+const miio = require('miio');
+
+module.exports = function (api) {
+    api.registerAccessory('homebridge-xiaomi-smart-plug', XiaomiSmartPlug);
+};
+
+class XiaomiSmartPlug {
+    constructor(log, config, api) {
+        // Setup configuration
+        this.log = log;
+        this.api = api;
+        this.config = config;
+
+        this.Service = this.api.hap.Service;
+        this.Characteristic = this.api.hap.Characteristic;
+
+        this.name = config['name'] || 'Mi smart plug';
+        if (!config['ip']) {
+            this.log('No IP address define for', this.name);
+            return;
+        }
+        if (!config['token']) {
+            this.log('No token define for', this.name);
+            return;
+        }
+        this.ip = config['ip'];
+        this.token = config['token'];
+
+        // Setup services
+        this.service = new this.Service(this.Service.Outlet);
+        this.service
+            .getCharacteristic(this.Characteristic.On)
+            .onGet(this.getState.bind(this))
+            .onSet(this.setState.bind(this));
+
+        this.checkPlugState().catch((error) => this.log.error(error));
+    }
+
+    async getDevice() {
+        if (this.device) return this.device;
+        this.log('Connect to device');
+        try {
+            this.device = await miio.device({
+                address: this.ip,
+                token: this.token
+            });
+        } catch (e) {
+            this.device = undefined;
+            this.log.error('Device not connected', e);
+        }
+        return this.device;
+    }
+
+    async checkPlugState() {
+        const device = await this.getDevice();
+        device
+            .miioCall('get_prop', ['power'])
+            .then((isOn) => {
+                this.service
+                    .getCharacteristic(Characteristic.On)
+                    .updateValue(isOn === 'on');
+            })
+            .catch((e) => {
+                throw e;
+            });
+    }
+
+    async getState(callback) {
+        this.log('Get state...');
+        try {
+            const device = await this.getDevice();
+            const power =
+                (await device.miioCall('get_prop', ['power'])) === 'on';
+            callback(null, power);
+        } catch (e) {
+            this.log.error('Error getting state', e);
+            callback(e);
+        }
+    }
+
+    async setState(state, callback) {
+        this.log('Set state to', state);
+        try {
+            const device = await this.getDevice();
+            await await device.miioCall('set_power', [state ? 'on' : 'off']);
+            callback(null);
+        } catch (e) {
+            this.log.error('Error setting state', e);
+            callback(e);
+        }
+    }
+
+    getServices() {
+        return [this.service];
+    }
+}
